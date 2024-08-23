520966fb496e862c8d5faf5bf89acfc4bdfd76c9
mxrsel <marsel14082006@gmail.com>
2024-08-23 20:19:44 +0600

    finished category router

--- routers/categories.ts
+++ routers/categories.ts
@@ -1,31 +1,69 @@
 import express from "express";
 import mysqlDb from "../mysqlDb";
-import {Category} from "../types";
+import {Category, CategoryMutation} from "../types";
+import {ResultSetHeader} from "mysql2";
 
 const categoriesRouter = express.Router();
 
-categoriesRouter.get('/', async (req, res) => {
-    const result = await mysqlDb.getConnection().query(
-        'SELECT * FROM categories'
-    );
-    const categories = result[0] as Category[];
-   return res.send(categories);
+categoriesRouter.get('/', async (req, res, next) => {
+    try {
+        const result = await mysqlDb.getConnection().query(
+            'SELECT * FROM categories'
+        );
+        const categories = result[0] as Category[];
+        return res.send(categories);
+    } catch (e) {
+        next(e)
+    }
 });
 
-categoriesRouter.get('/:id', async (req, res) => {
-    const id = req.params.id;
-    const result = await mysqlDb.getConnection().query(
-        'SELECT * FROM categories WHERE id = ?',
-        [id]
-    );
-    const categories = result[0] as Category[];
+categoriesRouter.get('/:id', async (req, res, next) => {
+        const id = req.params.id;
+    try {
+        const result = await mysqlDb.getConnection().query(
+            'SELECT * FROM categories WHERE id = ?',
+            [id]
+        );
+        const categories = result[0] as Category[];
 
-    if(categories.length > 0){
-        return res.status(404).send({error: 'Category not found'});
+        if (categories.length > 0) {
+            return res.status(404).send({error: 'Category not found'});
+        }
+        return res.send(categories[0]);
+    }catch (e) {
+        next(e)
+    }
+});
+
+categoriesRouter.post('/', async (req, res, next) => {
+
+        if (!req.body.name) {
+            return res.status(400).send({error: 'Name is required'});
+        }
+
+        const category: CategoryMutation = {
+            name: req.body.name,
+            description: req.body.description,
+        };
+        try {
+        const insertResult = await mysqlDb.getConnection().query(
+            'INSERT INTO categories (name, description) VALUES (?, ?)',
+            [category.name, category.description],
+        );
+
+        const resultHeader = insertResult[0] as ResultSetHeader
+
+        const getNewResult = await mysqlDb.getConnection().query(
+            'SELECT * FROM categories WHERE id = ?',
+            [resultHeader.insertId]
+        );
+
+        const categories = getNewResult[0] as Category[]
+        return res.send(categories[0]);
+    }catch (e) {
+        next(e)
     }
 
-    return res.send(categories[0]);
 });
-
 
 export default categoriesRouter
